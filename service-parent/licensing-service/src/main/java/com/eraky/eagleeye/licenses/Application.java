d55264d188ba44b7851bd10c0eff584c967e04b7
AhmedEraky <ahmed_eraky95@yahoo.com>
2019-11-02 15:41:30 +0200

    create Eureka Server

--- service-parent/licensing-service/src/main/java/com/eraky/eagleeye/licenses/Application.java
+++ service-parent/licensing-service/src/main/java/com/eraky/eagleeye/licenses/Application.java
@@ -2,9 +2,11 @@ package com.eraky.eagleeye.licenses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
